d750e110364ad8eced41f0d0ca86b93085c85bd3
Ekaterina <katya-tuvaeva@yandex.ru>
2023-12-04 18:13:25 +0300

    HW1

---
+++ HW1/task2.py
@@ -0,0 +1,15 @@
+from random import randint
+from typing import List
+
+
+def bubble_sort(arr: List[int]) -> List[int]:
+    if len(arr) == 0:
+        raise ValueError(
+             #На вход должен подаваться непустой целочисленный список
+            )
+    return sorted(arr, reverse=True)
+
+
+numbers = [randint(0, 100) for i in range(7)]
+print(numbers)
+print(bubble_sort(numbers))
